9c07ad8149524bc75c3f2ac28a11651c7911a525
Sanghyuk Jung <sanghyuk.jung@nhn.com>
2014-01-02 16:15:03 +0900

    rename local variables

--- jdk7-date-time/src/test/java/com/nbp/weblab/date/JodaTimeTest.java
+++ jdk7-date-time/src/test/java/com/nbp/weblab/date/JodaTimeTest.java
@@ -16,23 +16,23 @@ public class JodaTimeTest {
 	public void shouldGetAfterOneDay() {
 		Chronology chrono = GregorianChronology.getInstance();
 		LocalDate theDay = new LocalDate(1582, 10, 4, chrono);
-		String format = "yyyy.MM.dd";
-		assertThat(theDay.toString(format)).isEqualTo("1582.10.04");
+		String pattern = "yyyy.MM.dd";
+		assertThat(theDay.toString(pattern)).isEqualTo("1582.10.04");
 		
 		LocalDate nextDay = theDay.plusDays(1);
-		assertThat(nextDay.toString(format)).isEqualTo("1582.10.05");
+		assertThat(nextDay.toString(pattern)).isEqualTo("1582.10.05");
 	}
 	
 	@Test
 	public void shouldGetAfterOneHour() {
 		DateTimeZone seoul = DateTimeZone.forID("Asia/Seoul");
 		DateTime theTime = new DateTime(1988,5,7,23,0, seoul);
-		String format = "yyyy.MM.dd HH:mm";
-		assertThat(theTime.toString(format)).isEqualTo("1988.05.07 23:00");
+		String pattern = "yyyy.MM.dd HH:mm";
+		assertThat(theTime.toString(pattern)).isEqualTo("1988.05.07 23:00");
 		assertThat(seoul.isStandardOffset(theTime.getMillis())).isTrue();
 		
 		DateTime after1Hour = theTime.plusHours(1);
-		assertThat(after1Hour.toString(format)).isEqualTo("1988.05.08 01:00");
+		assertThat(after1Hour.toString(pattern)).isEqualTo("1988.05.08 01:00");
 		assertThat(seoul.isStandardOffset(after1Hour.getMillis())).isFalse();
 	}
 	
@@ -40,22 +40,22 @@ public class JodaTimeTest {
 	public void shouldGetAfterOneMinute() {
 		DateTimeZone seoul = DateTimeZone.forID("Asia/Seoul");
 		DateTime theTime = new DateTime(1961, 8, 9, 23, 59, seoul);
-		String format = "yyyy.MM.dd HH:mm";
-		assertThat(theTime.toString(format)).isEqualTo("1961.08.09 23:59");
+		String pattern = "yyyy.MM.dd HH:mm";
+		assertThat(theTime.toString(pattern)).isEqualTo("1961.08.09 23:59");
 		
 		DateTime after1Minute = theTime.plusMinutes(1);
-		assertThat(after1Minute.toString(format)).isEqualTo("1961.08.10 00:30");
+		assertThat(after1Minute.toString(pattern)).isEqualTo("1961.08.10 00:30");
 	}
 
 	@Test
 	public void shouldGetAfterTwoSecond() {
 		DateTimeZone utc = DateTimeZone.forID("UTC");
 		DateTime theTime = new DateTime(2012, 6, 30, 23, 59, 59, utc);
-		String format = "yyyy.MM.dd HH:mm:ss";
-		assertThat(theTime.toString(format)).isEqualTo("2012.06.30 23:59:59");
+		String pattern = "yyyy.MM.dd HH:mm:ss";
+		assertThat(theTime.toString(pattern)).isEqualTo("2012.06.30 23:59:59");
 		
 		DateTime after2Seconds = theTime.plusSeconds(2);
-		assertThat(after2Seconds.toString(format)).isEqualTo("2012.07.01 00:00:01");
+		assertThat(after2Seconds.toString(pattern)).isEqualTo("2012.07.01 00:00:01");
 	}
 
 	@Test(expected=IllegalArgumentException.class)
--- jdk7-date-time/src/test/java/com/nbp/weblab/date/OldJdkDateTest.java
+++ jdk7-date-time/src/test/java/com/nbp/weblab/date/OldJdkDateTest.java
@@ -16,12 +16,12 @@ public class OldJdkDateTest {
 		TimeZone utc = TimeZone.getTimeZone("UTC");
 		Calendar calendar = Calendar.getInstance(utc);
 		calendar.set(1582, Calendar.OCTOBER , 4);
-		String format = "yyyy.MM.dd";
-		String theDay = toString(calendar, format, utc);
+		String pattern = "yyyy.MM.dd";
+		String theDay = toString(calendar, pattern, utc);
 		assertThat(theDay).isEqualTo("1582.10.04");
 
 		calendar.add(Calendar.DATE, 1);
-		String nextDay = toString(calendar, format, utc);
+		String nextDay = toString(calendar, pattern, utc);
 		assertThat(nextDay).isEqualTo("1582.10.15");
 	}
 	
@@ -30,13 +30,13 @@ public class OldJdkDateTest {
 		TimeZone seoul = TimeZone.getTimeZone("Asia/Seoul");
 		Calendar calendar = Calendar.getInstance(seoul);
 		calendar.set(1988, Calendar.MAY , 7, 23, 0);
-		String format = "yyyy.MM.dd HH:mm";
-		String theTime = toString(calendar, format, seoul);
+		String pattern = "yyyy.MM.dd HH:mm";
+		String theTime = toString(calendar, pattern, seoul);
 		assertThat(seoul.inDaylightTime(calendar.getTime())).isFalse();
 		assertThat(theTime).isEqualTo("1988.05.07 23:00");
 		
 		calendar.add(Calendar.HOUR_OF_DAY, 1);
-		String after1Hour = toString(calendar, format, seoul);
+		String after1Hour = toString(calendar, pattern, seoul);
 		assertThat(seoul.inDaylightTime(calendar.getTime())).isTrue();
 		assertThat(after1Hour).isEqualTo("1988.05.08 01:00");
 	}
@@ -46,12 +46,12 @@ public class OldJdkDateTest {
 		TimeZone seoul = TimeZone.getTimeZone("Asia/Seoul");
 		Calendar calendar = Calendar.getInstance(seoul);
 		calendar.set(1961, Calendar.AUGUST, 9, 23, 59);
-		String format = "yyyy.MM.dd HH:mm";
-		String theTime = toString(calendar, format, seoul);
+		String pattern = "yyyy.MM.dd HH:mm";
+		String theTime = toString(calendar, pattern, seoul);
 		assertThat(theTime).isEqualTo("1961.08.09 23:59");
 		
 		calendar.add(Calendar.MINUTE, 1);
-		String after1Minute = toString(calendar, format, seoul);
+		String after1Minute = toString(calendar, pattern, seoul);
 		assertThat(after1Minute).isEqualTo("1961.08.10 00:30");
 	}
 
@@ -60,11 +60,12 @@ public class OldJdkDateTest {
 		TimeZone utc = TimeZone.getTimeZone("UTC");
 		Calendar calendar = Calendar.getInstance(utc);
 		calendar.set(2012, Calendar.JUNE, 30, 23, 59, 59);
-		String theTime = toString(calendar, "yyyy.MM.dd HH:mm:ss", utc);
+		String pattern = "yyyy.MM.dd HH:mm:ss";
+		String theTime = toString(calendar, pattern, utc);
 		assertThat(theTime).isEqualTo("2012.06.30 23:59:59");
 		
 		calendar.add(Calendar.SECOND, 2);
-		String afterTwoSeconds = toString(calendar, "yyyy.MM.dd HH:mm:ss", utc);
+		String afterTwoSeconds = toString(calendar, pattern, utc);
 		assertThat(afterTwoSeconds).isEqualTo("2012.07.01 00:00:01");
 	}
 	
