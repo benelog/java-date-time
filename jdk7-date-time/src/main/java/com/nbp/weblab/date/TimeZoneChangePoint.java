634a6cbaa77241d9bc97c4b9220736788bca77d5
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2015-02-06 06:12:33 +0900

    섬머타임 시간 출력 추가

--- jdk7-date-time/src/main/java/com/nbp/weblab/date/TimeZoneChangePoint.java
+++ jdk7-date-time/src/main/java/com/nbp/weblab/date/TimeZoneChangePoint.java
@@ -1,17 +1,21 @@
 package com.nbp.weblab.date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class TimeZoneChangePoint {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		printTime(1954,  3, 20, 22, 59);
 		printTime(1954,  3, 20, 23,  0);
 		printTime(1961,  8,  9, 23, 59);
 		printTime(1961,  8, 10,  0,  0);
 		printTime(1968,  9, 30, 23, 59);
 		printTime(1968, 10,  1,  0,  0);
+		printTime(1987,  5, 10,  1,  0);
+		printTime(1988,  5,  8,  1,  0);
 	}
 
 	private static void printTime(int year, int month, int day, int hour, int second) {
@@ -19,10 +23,10 @@ public class TimeZoneChangePoint {
 		Calendar calendar = Calendar.getInstance(zone);
 		calendar.set(year, month - 1, day, hour, second);
 
-		String pattern = "yyyy.MM.dd HH:mm (Z)";
+		String pattern = "yyyy.MM.dd HH:mm (z Z)";
 		SimpleDateFormat format = new SimpleDateFormat(pattern);
 		format.setTimeZone(zone);
-		
+
 		System.out.println(format.format(calendar.getTime()));
 	}
 }
